a343325099b7d48b33a5eae30dead1bb0d2bfd69
Anshita Garg <anshita14garg@gmail.com>
2026-09-17 12:22:00 +0530

    lab4

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -19,9 +19,13 @@ const server = http.createServer((req, res) => {
         })
         res.end(JSON.stringify([{ msg: "add user" }]));
     }
-    else if (req.url === "/api/users/1" && req.method === "GET") {
-        res.end(JSON.stringify([{ msg: "single user with id 1" }]));
+    else if (req.url.startsWith("/api/users/1") && req.method === "GET") {
+
+        const userId = req.url.split("/").pop();
+
+        res.end(JSON.stringify({ msg: `showing details of  user with id  ${userId}` }));
     }
+
     else if (req.url === "/api/users/1" && req.method === "PUT") {
         res.end(JSON.stringify([{ msg: "update user with id 1" }]));
     }
